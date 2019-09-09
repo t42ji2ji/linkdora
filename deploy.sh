f32083e95949a1b0c5058338ef18b456ca384a9a
DORA_XIE <joy8963259@gmail.com>
2019-09-09 18:14:14 +0800

    dist

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 cd dist
+git init
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push origin gh-pages
+git push -f https://github.com/t42ji2ji/linkdora.git master:gh-pages
 cd -
